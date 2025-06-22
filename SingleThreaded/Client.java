40fd4e5759502d6db4f1c73ea16426bfc838f1c4
Shash <ashishsinghlogic@gmail.com>
2025-06-22 09:50:14 +0530

    comments

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -10,16 +10,34 @@ import java.net.UnknownHostException;
 public class Client {
     
     public void run() throws UnknownHostException, IOException{
-        int port = 8090;
+        int port = 8010;
         InetAddress address = InetAddress.getByName("localhost");
+        //InetAddress is used to get the IP address of the server locally
+        //getByName("localhost") returns the IP address of the server locally
+        //port is the port number of the server
         Socket socket = new Socket(address, port);
+        //Socket is used to connect to the server
+        //address is the IP address of the server
         PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        //print writer is used to send data to the server
+        //true is used to flush the output stream
+        //by flushing the output stream, the data is sent to the server
+        //socket.getOutputStream() returns the output stream of the server
         BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        //buffered reader is used to read data from the server
+        //new InputStreamReader(socket.getInputStream()) returns the input stream of the server
         toSocket.println("Hello World from socket "+socket.getLocalSocketAddress());
+        //println is used to send data to the server
+        //socket.getLocalSocketAddress() returns the local socket address of the client
         String line = fromSocket.readLine();
+        //readLine is used to read data from the server
+        //fromSocket.readLine() returns the data from the server
         toSocket.close();
+        //close is used to close the output stream
         fromSocket.close();
+        //close is used to close the input stream
         socket.close();
+        //close is used to close the socket
     }
     
     public static void main(String[] args) {
--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -8,17 +8,60 @@ import java.net.UnknownHostException;
 
 public class Server {
     
+    //This model is simple to understand but impractical for real-world use.
+    //If one client connects and the server is doing some time-consuming work, all other clients have to wait.
+    //This is because the server is single-threaded and can only handle one client at a time.
+    //This is not efficient and is not scalable.
+    //This is why we need to use a multi-threaded server.
+    //A multi-threaded server can handle multiple clients at the same time.
+    //This is more efficient and is scalable.
+    //This is why we need to use a multi-threaded server.
+    
     public void run() throws IOException, UnknownHostException{
+        //we are throwing an exception because we are not handling it
+        //we are not handling the exception because we are not using a try-with-resources block
+
+        //IOE exception is thrown when there is an error in the input/output stream
+        //UnknownHostException is thrown when the host is not found
+
+        //listening on port 8010
         int port = 8010;
+
+        //creating a server socket
         ServerSocket socket = new ServerSocket(port);
+        
+        //setting a timeout of 20 seconds
         socket.setSoTimeout(20000);
+
+        //infinite loop to accept connections
         while(true){
+            //printing the port number
             System.out.println("Server is listening on port: "+port);
+
+            //accepting a connection from the client
             Socket acceptedConnection = socket.accept();
+
+            //printing the remote socket address
             System.out.println("Connected to "+acceptedConnection.getRemoteSocketAddress());
+            //getRemoteSocketAddress() returns the remote socket address of the client
+            //getInetAddress() returns the local socket address of the server
+            //getPort() returns the port number of the client
+            //getAddress() returns the IP address of the client
+            //getPort() returns the port number of the server
+            //getAddress() returns the IP address of the server
+            //getInetAddress() returns the local socket address of the server
+
+            //creating a print writer to send data to the client
             PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+            //print writer is used to send data to the client
+            //true is used to flush the output stream
+            //acceptedConnection.getOutputStream() returns the output stream of the client
+            //acceptedConnection.getInputStream() returns the input stream of the client
             BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            //buffered reader is used to read data from the client
+            //new InputStreamReader(acceptedConnection.getInputStream()) returns the input stream of the client
             toClient.println("Hello World from the server");
+            //print writer is used to send data to the client
         }
     }
 
